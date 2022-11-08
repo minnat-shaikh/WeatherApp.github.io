d7c54d9d38685fb683cf5f96f042581956b6fd06
Minnat Shaikh <117262525+minnat-shaikh@users.noreply.github.com>
2022-11-08 12:39:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ searchInputBox.addEventListener("keypress", (event) => {
 
     const sample = async () => {
       document.querySelector(".info-txt").style.display = "block";
-      let delayres = await delay(950);
+      let delayres = await delay(1500);
       document.querySelector(".info-txt").style.display = "none";
     };
     sample();
